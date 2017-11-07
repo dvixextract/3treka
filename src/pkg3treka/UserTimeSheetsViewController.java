4a81c4adc1d3e1bbab33b60439723428f258c7e6
kudzaiTnexus <kudzimhou@gmail.com>
2017-11-07 08:23:34 +0200

    added new viewcontroller for usertimesheets and modified user timesheets view

---
+++ src/pkg3treka/UserTimeSheetsViewController.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg3treka;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+
+/**
+ *
+ * @author User
+ */
+public class UserTimeSheetsViewController  implements Initializable{
+    
+    @FXML
+    private TableColumn<?, ?> projectCodeColumnProjectListTable;
+
+    @FXML
+    private TableColumn<?, ?> projectNameColumnProjectListTable;
+
+    @FXML
+    private TableColumn<?, ?> taskCodeColumnProjectListTable;
+
+    @FXML
+    private TableColumn<?, ?> taskNameColumnProjectListTable;
+
+    @FXML
+    private ListView<?> teamMemberOfListview;
+
+    @FXML
+    private TableColumn<?, ?> projectNumberColumnTimeSheetTable;
+
+    @FXML
+    private TableColumn<?, ?> projectNameColumnTimeSheetTable;
+
+    @FXML
+    private TableColumn<?, ?> taskCodeColumnTimeSheetTable;
+
+    @FXML
+    private TableColumn<?, ?> taskNameColumnTimeSheetTable;
+
+    @FXML
+    private TableColumn<?, ?> taskDescriptionColumnTimeSheetTable;
+
+    @FXML
+    private TableColumn<?, ?> dayColumnTimesheetTable;
+
+    @FXML
+    private TableColumn<?, ?> projectNumberColumnTableProject;
+
+    @FXML
+    private TableColumn<?, ?> projectNameColumnTableProject;
+
+    @FXML
+    private TableColumn<?, ?> taskCodeColumnTableTask;
+
+    @FXML
+    private TableColumn<?, ?> taskNameColumnTableTask;
+
+    @FXML
+    private TableColumn<?, ?> taskDescriptionColumnTableTask;
+
+    @FXML
+    private Button startTimerButton;
+
+    @FXML
+    private Button pauseTimerButton;
+
+    @FXML
+    private Button stopTimerButton;
+
+    @FXML
+    void handlePauseTimerButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleStartTimerButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleStopTimerButton(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+    
+}
